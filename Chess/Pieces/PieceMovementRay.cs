ff9b2c56bb01aa8b3649e80f60a9510d681be6f3
.haise_san <98202347+Haise777@users.noreply.github.com>
2024-01-29 02:18:16 -0300

    massive refactoring on 'PieceMovementRay'

--- Chess/Pieces/PieceMovementRay.cs
+++ Chess/Pieces/PieceMovementRay.cs
@@ -1,74 +1,95 @@
+using LineDataTuple = ( //Alias for tuple
+    Chess.Pieces.IRayPiece currentPiece,
+    System.Collections.Generic.List<Chess.Square> validatedSquares,
+    Chess.Square[] allSquares);
+
 namespace Chess.Pieces;
 
 public static class PieceMovementRay
 {
-    public static Square[] GetPlusSquares(this IRayPiece piece, int position, Square[] squares)
+    public static Square[] GetPlusSquares(this IRayPiece currentPiece, int startingPosition, Square[] allSquares)
     {
-        var rays = piece.SquaresInSight.Keys.Take(4).ToArray();
+        var tracesIndex = currentPiece.SquaresInSight.Keys.Take(4).ToArray();
+        var validatedSquares = new List<Square>();
 
-        var squaresToMove = new List<Square>();
+        var up = new TracingData(startingPosition + 8);
+        var down = new TracingData(startingPosition - 8);
+        var left = new TracingData(startingPosition - 1);
+        var right = new TracingData(startingPosition + 1);
 
-        var up = position + 8;
-        var down = position - 8;
-        var left = position - 1;
-        var right = position + 1;
+        var lineData = (currentPiece, validatedSquares, allSquares);
 
-        var keepUp = true;
-        var keepDown = true;
-        var keepLeft = true;
-        var keepRight = true;
-
-        Piece? upPiece = null;
-        Piece? downPiece = null;
-        Piece? leftPiece = null;
-        Piece? rightPiece = null;
-
-        int n;
+        bool keepChecking;
         do
         {
-            n = 0;
-            if (up < 64)
-            {
-                CheckSquare(0, piece, squaresToMove, squares[up], ref keepUp, ref upPiece);
+            keepChecking = false;
 
-                piece.SquaresInSight[rays[0]].Add(squares[up]);
-                up += 8;
-                n++;
+            CheckTraceLine(0, up.CurrentPosition < 64, num => num + 8, up, lineData, ref keepChecking);
+            CheckTraceLine(1, down.CurrentPosition >= 0, num => num - 8, down, lineData, ref keepChecking);
+            CheckTraceLine(2, (left.CurrentPosition + 1) % 8 != 0, num => --num, left, lineData, ref keepChecking);
+            CheckTraceLine(3, right.CurrentPosition % 8 != 0, num => ++num, right, lineData, ref keepChecking);
+        } while (keepChecking);
+
+        return validatedSquares.ToArray();
+    }
+    
+    public static Square[] GetCrossSquares(this IRayPiece currentPiece, int startingPosition, Square[] allSquares)
+    {
+        var tracesIndex = currentPiece.SquaresInSight.Count == 4
+            ? currentPiece.SquaresInSight.Keys.Take(4).ToArray()
+            : currentPiece.SquaresInSight.Keys.TakeLast(4).ToArray();
+
+        var validatedSquares = new List<Square>();
+
+        var upperRight = new TracingData(startingPosition + 9);
+        var upperLeft = new TracingData(startingPosition + 7);
+        var lowerRight = new TracingData(startingPosition - 9);
+        var lowerLeft = new TracingData(startingPosition - 7);
+
+        var lineData = (currentPiece, validatedSquares, allSquares);
+
+        bool keepChecking;
+        do
+        {
+            keepChecking = false;
+
+            if (upperRight.CurrentPosition < 64 && upperLeft.CurrentPosition < 64)
+            {
+                CheckTraceLine(0, upperRight.CurrentPosition % 8 != 0, num => num + 9, upperRight, lineData,
+                    ref keepChecking);
+                CheckTraceLine(1, (upperLeft.CurrentPosition + 1) % 8 != 0, num => num + 7, upperLeft, lineData,
+                    ref keepChecking);
             }
 
-            if (down >= 0)
+            if (lowerRight.CurrentPosition >= 0 && lowerLeft.CurrentPosition >= 0)
             {
-                CheckSquare(1, piece, squaresToMove, squares[down], ref keepDown, ref downPiece);
-
-                piece.SquaresInSight[rays[1]].Add(squares[down]);
-                down -= 8;
-                n++;
+                CheckTraceLine(2, lowerRight.CurrentPosition % 8 != 0, num => num - 9, lowerRight, lineData,
+                    ref keepChecking);
+                CheckTraceLine(3, (lowerLeft.CurrentPosition + 1) % 8 != 0, num => num - 7, lowerLeft, lineData,
+                    ref keepChecking);
             }
+        } while (keepChecking);
 
-            if ((left + 1) % 8 != 0)
-            {
-                CheckSquare(2, piece, squaresToMove, squares[left], ref keepLeft, ref leftPiece);
+        return validatedSquares.ToArray();
+    }
+    
+    private static void CheckTraceLine(int traceIndex, bool condition, Func<int, int> operation,
+        TracingData tracingData, LineDataTuple ldt, ref bool keepChecking)
+    {
+        if (!condition) return;
+        CheckSquare(traceIndex, ldt.currentPiece, ldt.validatedSquares, ldt.allSquares[tracingData.CurrentPosition],
+            ref tracingData.ShouldKeepGoing,
+            ref tracingData.FoundPiece);
 
-                piece.SquaresInSight[rays[2]].Add(squares[left]);
-                left--;
-                n++;
-            }
+        ldt.currentPiece.SquaresInSight[traceIndex].Add(ldt.allSquares[tracingData.CurrentPosition]);
+        tracingData.CurrentPosition = operation(tracingData.CurrentPosition);
 
-            if (right % 8 != 0)
-            {
-                CheckSquare(3, piece, squaresToMove, squares[right], ref keepRight, ref rightPiece);
-
-                piece.SquaresInSight[rays[3]].Add(squares[right]);
-                right++;
-                n++;
-            }
-        } while (n != 0);
-
-        return squaresToMove.ToArray();
+        keepChecking = true;
     }
 
     private static void CheckSquare(
-        int rayNum, IRayPiece piece, List<Square> squaresToMove, Square currentSquare, ref bool keep, ref Piece? foundPiece)
+        int rayNum, IRayPiece piece, List<Square> squaresToMove, Square currentSquare, ref bool keep,
+        ref Piece? foundPiece)
     {
         if (currentSquare.Piece is not null && currentSquare.Piece.Color == piece.Color)
             keep = false;
@@ -80,7 +101,6 @@ public static class PieceMovementRay
                 foundPiece = currentSquare.Piece;
                 squaresToMove.Add(currentSquare);
             }
-            
         }
         else if (keep)
         {
@@ -91,81 +111,5 @@ public static class PieceMovementRay
             }
             else keep = false;
         }
-    }
-
-    public static Square[] GetCrossSquares(this IRayPiece piece, int position, Square[] squares)
-    {
-        var rays = piece.SquaresInSight.Count == 4
-            ? piece.SquaresInSight.Keys.Take(4).ToArray()
-            : piece.SquaresInSight.Keys.TakeLast(4).ToArray();
-
-        var squaresToMove = new List<Square>();
-
-        var upperRight = position + 9;
-        var upperLeft = position + 7;
-        var lowerRight = position - 9;
-        var lowerLeft = position - 7;
-
-        var keepLowerLeft = true;
-        var keepLowerRight = true;
-        var keepUpperLeft = true;
-        var keepUpperRight = true;
-        
-        Piece? upperRightPiece = null;
-        Piece? upperLeftPiece = null;
-        Piece? lowerRightPiece = null;
-        Piece? lowerLeftPiece = null;
-
-        int n;
-        do
-        {
-            n = 0;
-            //Checks the upper limit
-            if (upperRight < 64 || upperLeft < 64)
-            {
-                //Checks the lateral limits
-                if (upperRight % 8 != 0)
-                {
-                    CheckSquare(0, piece, squaresToMove, squares[upperRight], ref keepUpperRight, ref upperRightPiece);
-
-                    piece.SquaresInSight[rays[0]].Add(squares[upperRight]);
-                    upperRight += 9;
-                    n++;
-                }
-
-                if ((upperLeft + 1) % 8 != 0)
-                {
-                    CheckSquare(1, piece, squaresToMove, squares[upperLeft], ref keepUpperLeft, ref upperLeftPiece);
-
-                    piece.SquaresInSight[rays[1]].Add(squares[upperLeft]);
-                    upperLeft += 7;
-                    n++;
-                }
-            }
-
-            if (lowerRight >= 0 || lowerLeft >= 0)
-            {
-                //Checks the lateral limits
-                if (lowerRight % 8 != 0)
-                {
-                    CheckSquare(2, piece, squaresToMove, squares[lowerRight], ref keepLowerRight, ref lowerRightPiece);
-
-                    piece.SquaresInSight[rays[2]].Add(squares[lowerRight]);
-                    lowerRight -= 9;
-                    n++;
-                }
-
-                if ((lowerLeft + 1) % 8 != 0)
-                {
-                    CheckSquare(3, piece, squaresToMove, squares[lowerLeft], ref keepLowerLeft, ref lowerLeftPiece);
-
-                    piece.SquaresInSight[rays[3]].Add(squares[lowerLeft]);
-                    lowerLeft -= 7;
-                    n++;
-                }
-            }
-        } while (n != 0);
-
-        return squaresToMove.ToArray();
     }
 }
---
+++ Chess/Pieces/TracingData.cs
@@ -0,0 +1,8 @@
+﻿namespace Chess.Pieces;
+
+public class TracingData(int currentPosition)
+{
+    public int CurrentPosition = currentPosition;
+    public bool ShouldKeepGoing = true;
+    public Piece? FoundPiece = null;
+}
